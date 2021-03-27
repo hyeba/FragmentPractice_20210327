e52c7d6a9b6cac51d5d95b77a6e0417baaaf398d
장혜진 <aslk3366zz@naver.com>
2021-03-27 12:35:48 +0900

    두번째 프래그먼트 생성

---
+++ app/src/main/java/com/neppplus/fragmentpractice_20210327/fragments/SecondFragment.kt
@@ -0,0 +1,36 @@
+package com.neppplus.fragmentpractice_20210327.fragments
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.neppplus.fragmentpractice_20210327.R
+import kotlinx.android.synthetic.main.fragment_second.*
+
+class SecondFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_second, container, false)
+
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        slogBtn.setOnClickListener {
+
+         Log.d("아무거나", "로그출력")
+
+        }
+
+    }
+
+}
